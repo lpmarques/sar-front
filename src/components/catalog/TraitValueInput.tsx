c8da0eb5f0d7113e4315396262b89b6d34fec9cd
Lucas Marques <perma.lucas@gmail.com>
2025-09-29 21:24:16 -0300

    implement two-format input for range trait values

--- src/components/catalog/TraitValueInput.tsx
+++ src/components/catalog/TraitValueInput.tsx
@@ -1,10 +1,11 @@
-import { Center, Container, MultiSelect, NumberInput, RangeSlider, Select, Switch, Text } from '@mantine/core';
+import { Center, Container, Group, MultiSelect, NativeSelect, NumberInput, RangeSlider, Select, Switch, Text, TextInput } from '@mantine/core';
 import { UseFieldReturnType } from '@mantine/form';
 import {
   Range,
   TraitReadData,
   TraitValue,
 } from "../../apis/catalog";
+import { useState } from 'react';
 
 interface TraitValueInputProps {
   trait: TraitReadData,
@@ -40,6 +41,7 @@ function StringTraitSelect({ trait, field, ...extraSelectProps }: { trait: Trait
         key={field.key}
         data={options}
         searchable
+        withScrollArea={false}
         {...field.getInputProps()}
         {...extraSelectProps}
       />
@@ -101,6 +103,7 @@ function StringArrayTraitSelect({ trait, field, ...extraSelectProps }: { trait: 
         key={field.key}
         data={options}
         searchable
+        withScrollArea={false}
         {...field.getInputProps()}
         {...extraSelectProps}
         />
@@ -109,34 +112,85 @@ function StringArrayTraitSelect({ trait, field, ...extraSelectProps }: { trait: 
 }
 
 function RangeTraitSlider({ trait, field, ...extraSliderProps }: { trait: TraitReadData, field: UseFieldReturnType<Range> }) {  
-  const min = field.getValue()[0];
-  const max = field.getValue()[1];
-  const step = Number.isInteger(min) ? 1 : 0.1;
-  const minRange = 0;
+  const initialMin = field.getValue()[0];
+  const initialMax = field.getValue()[1];
+  const step = Number.isInteger(initialMin) ? 1 : 0.1; // TODO: somehow get this info from backend
+  const minRangeLength = 0;
+  console.log(initialMin)
+
+  const changeRangeMin = (value: number) => {
+    if (!Number.isNaN(value)) {
+      field.setValue([value, field.getValue()[1]]);
+    }
+  }
+
+  const limitRangeMin = (value: number) => {
+    const currMax = field.getValue()[1];
+    if (currMax-value < minRangeLength)
+      field.setValue([currMax-minRangeLength, currMax]);
+  }
+
+  const changeRangeMax = (value: number) => {
+    if (!Number.isNaN(value)) {
+      field.setValue([field.getValue()[0], value]);
+    }
+  }
+
+  const limitRangeMax = (value: number) => {
+    const currMin = field.getValue()[0];
+    if (value-currMin < minRangeLength)
+      field.setValue([currMin, currMin+minRangeLength]);
+    console.log(field.getValue())
+  }
 
   return (
-    <>
-    <RangeSlider
-      key={field.key}
-      size="sm"
-      domain={[trait.numericValueMin, trait.numericValueMax]}
-      min={trait.numericValueMin}
-      max={trait.numericValueMax}
-      marks={[
-        { value: min },
-        { value: max },
-      ]}
-      step={step}
-      minRange={minRange}
-      color="gray.5"
-      label={(value: number) => value}
-      labelAlwaysOn
-      {...field.getInputProps()}
-      {...extraSliderProps}
-      p={20}
-      mt={30}
-    />
-    <Text size="xs" c="red">{field.getInputProps().error}</Text>
-    </>
+    <div key={field.key}>
+      <RangeSlider
+        size="sm"
+        domain={[trait.numericValueMin, trait.numericValueMax]}
+        min={trait.numericValueMin}
+        max={trait.numericValueMax}
+        marks={[
+          { value: initialMin },
+          { value: initialMax },
+        ]}
+        step={step}
+        minRange={minRangeLength}
+        color="gray.5"
+        label={(value: number) => value}
+        labelAlwaysOn
+        {...field.getInputProps()}
+        {...extraSliderProps}
+        p={20}
+        mt={30}
+      />
+      <Group justify='center'>
+        <Container size={100} m={0} p={0}>
+          <NumberInput
+            defaultValue={String(initialMin)}
+            min={trait.numericValueMin}
+            max={trait.numericValueMax}
+            value={String(field.getValue()[0])}
+            onChange={(e) => changeRangeMin(Number(e))}
+            onBlur={(e) => limitRangeMin(Number(e.currentTarget.value))}
+            size="xs"
+          />
+          <Text c="dimmed" fz={14}>Mín.</Text>
+        </Container>
+        <Container size={100} m={0} p={0}>
+          <NumberInput
+            defaultValue={String(initialMax)}
+            min={trait.numericValueMin}
+            max={trait.numericValueMax}
+            value={String(field.getValue()[1])}
+            onChange={(e) => changeRangeMax(Number(e))}
+            onBlur={(e) => limitRangeMax(Number(e.currentTarget.value))}
+            size="xs"
+          />
+          <Text c="dimmed" fz={14}>Máx.</Text>
+        </Container>
+      </Group>
+      <Text size="xs" c="red">{field.getInputProps().error}</Text>
+    </div>
   )
 }
