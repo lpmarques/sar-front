6259f1d282fd6c34dcb1868202cc5e96615e0c2c
Lucas Marques <perma.lucas@gmail.com>
2026-03-19 13:39:22 -0300

    add licensing notice headers

--- src/components/agroforestry/MapCentering.tsx
+++ src/components/agroforestry/MapCentering.tsx
@@ -1,3 +1,16 @@
+/*
+Simulador Agroflorestal Regenera (SAR)
+Copyright (C) 2026  Lucas Marques and Regenera Mata Atlântica
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses>.
+*/
+
 import { LatLngExpression } from 'leaflet';
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
